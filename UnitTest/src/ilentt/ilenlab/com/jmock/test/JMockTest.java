950251dff5018d19f5b0fc466c2bc4a03dfc5bb2
ilentt <ilentt@ilenlab.com>
2015-09-13 13:58:11 +0700

    update jmock

--- UnitTest/src/ilentt/ilenlab/com/jmock/test/JMockTest.java
+++ UnitTest/src/ilentt/ilenlab/com/jmock/test/JMockTest.java
@@ -1,13 +1,26 @@
 package ilentt.ilenlab.com.jmock.test;
 
+import junit.framework.Assert;
+
 import org.jmock.Mockery;
 import org.junit.Test;
 
+@SuppressWarnings("deprecation")
 public class JMockTest {
 
 	@Test
 	public void test() {
-		Mockery testInterfaceMock = new Mockery();
+		Mockery mockery = new Mockery();
+		final Math math = mockery.mock(Math.class);
+		mockery.checking(new org.jmock.Expectations()
+		{{
+			oneOf(math).Add(1,2);
+			will(returnValue((3)));
+		}});
+		
+		int i = math.Add(1, 2);
+		System.out.println(i);
+		Assert.assertEquals(i, 3);
 	}
 	
 
